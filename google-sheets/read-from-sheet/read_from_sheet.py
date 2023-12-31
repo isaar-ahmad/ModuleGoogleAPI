ddd5e0f1d7c9994f8e86335e70a93a33ca8edd03
Isaar Ahmad <isaar.ahmad@gmail.com>
2023-12-31 15:50:52 +0530

    read_from_sheet support : output csv files will be written to data/ folder

--- google-sheets/read-from-sheet/read_from_sheet.py
+++ google-sheets/read-from-sheet/read_from_sheet.py
@@ -13,44 +13,43 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from credentials.credential_manager import credential_management
 import json
-
+import pandas as pd
 # Read the ID and range of a sample spreadsheet from config.json
-config_json = open("google-sheets/read_from_sheet_config.json")
+config_json = open("google-sheets/read-from-sheet/read_from_sheet_config.json")
 config_data = json.load(config_json)
 SAMPLE_SPREADSHEET_ID = config_data["READ_SPREADSHEET_ID"]
 SAMPLE_RANGE_NAME = config_data["READ_RANGE"]
+DATA_WORKING_DIR = str("google-sheets/read-from-sheet/") + str(config_data["DATA_WORKING_DIR"])
 
 
 def main():
-  """Shows basic usage of the Sheets API.
-  Prints values from a sample spreadsheet.
-  """
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = credential_management()
 
-  creds = credential_management()
+    try:
 
-  try:
-    service = build("sheets", "v4", credentials=creds)
+        service = build("sheets", "v4", credentials=creds)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-        .execute()
-    )
-    values = result.get("values", [])
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
+        if not values:
+            print("No data found.")
+            return
 
-    if not values:
-      print("No data found.")
-      return
+        df = pd.DataFrame(values)
+        df.to_csv(str(DATA_WORKING_DIR)+"/"+str("out.csv"), index=False, header=False)
 
-    print("Name, Major:")
-    for row in values:
-      # Print columns A and E, which correspond to indices 0 and 4.
-      print(f"{row[0]}, {row[4]}")
-  except HttpError as err:
-    print(err)
+    except HttpError as err:
+        print(err)
 
 
 if __name__ == "__main__":
-  main()
+    main()
